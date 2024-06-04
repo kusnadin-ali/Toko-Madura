587b1c28b00593d240fc3659f15488863e78dcf6
kusnadin.ali <kusnadin.ali.12@gmail.com>
2024-06-04 11:52:03 +0700

    Update Back End

---
+++ Be-Toko/src/main/java/com/tujuhsembilan/tokomadura/TokomaduraApplication.java
@@ -0,0 +1,13 @@
+package com.tujuhsembilan.tokomadura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokomaduraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokomaduraApplication.class, args);
+	}
+
+}
---
+++ Be-Toko/src/test/java/com/tujuhsembilan/tokomadura/TokomaduraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tujuhsembilan.tokomadura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokomaduraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
